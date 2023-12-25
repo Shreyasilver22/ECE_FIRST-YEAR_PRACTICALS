2089d144f623bb11904808ccc6d0bb0dcd76f875
Shreyas Singh <148425369+Shreyasilver22@users.noreply.github.com>
2023-12-25 22:55:09 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,34 +1,45 @@
 /*2. Temperature Conversion: Write a program that uses two functions to convert temperature from Celsius 
 to Fahrenheit and vice versa, respectively and return the converted values. */
 
-#include<stdio.h>
-void Fahrenheit(float x);
-void Celsius(float y);
+#include <stdio.h>
+
+// Function to convert Celsius to Fahrenheit
+float celsiusToFahrenheit(float celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+// Function to convert Fahrenheit to Celsius
+float fahrenheitToCelsius(float fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
 int main()
 {
-    float a,b;
-    printf("enter celsius temperature that has to be changed into fahrenheit");
-    printf("enter fahrenheit temperature that has to be changed into celsius:");
-    scanf("%f%f",&a,&b);
-    Fahrenheit(a);
     
-    Celsius(b);
-
+    float temperature, convertedTemperature;
+    int choice;
 
     
-}
+    printf("Enter temperature value: ");
+    scanf("%f", &temperature);
 
-void Fahrenheit(float x)
-{
-    float c;
-    c=(((x-32)*5)/9);
-    scanf("%f",&c);
-    printf("temperature in farenheit is %d",c);
-}
+    
+    printf("Choose conversion:\n1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius\n");
+    scanf("%d", &choice);
 
-void Celsius(float y)
-{
-    float d;
+    
+    if (choice == 1) {
+        convertedTemperature = celsiusToFahrenheit(temperature);
+        printf("%.2f Celsius is %.2f Fahrenheit\n", temperature, convertedTemperature);
+    } else if (choice == 2) {
+convertedTemperature = fahrenheitToCelsius(temperature);
+        printf("%.2f Fahrenheit is %.2f Celsius\n", temperature, convertedTemperature);
+    } else {
+        printf("Invalid choice\n");
+    }
+
+    return 0;
+}
     
 
     d=((y*9)/5) + 32;
