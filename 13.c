611a240bfb3b550da749a688203db917dd7f9bab
Shreyas Singh <148425369+Shreyasilver22@users.noreply.github.com>
2023-12-25 22:44:56 +0530

    Update 13.c

--- 13.c
+++ 13.c
@@ -1,24 +1,22 @@
-/*
-String Reverse without Library Function: Write a program to reverse a string (array of
-char) without using string library functions.
+// String Reverse without Library Function: Write a program to reverse a string (array of char) without using string library functions.
 
-*/
 #include<stdio.h>
-int main()
-{
-    char a[50],b[50];
-    int i=0;
-    printf("enter a string:");
 
-    printf("\n");
+int main(){
+    char str[] = "String Reverse without Library Function";
+    int n = 0;
 
-    scanf("%s",a);
-    for(i=4;i>0;){
-        printf("%c",a[i]);
-       i=i-1;
+    while(str[n] != '\0'){
+        n++;
     }
-    
-    
 
+    for (int i = 0; i < n/2; i++){
+        char temp = str[i];
+        str[i] = str[n - 1 - i];
+        str[n - 1 - i] = temp;
+    }
 
+    printf("%s", str);
+    
+    return 0;
 }
